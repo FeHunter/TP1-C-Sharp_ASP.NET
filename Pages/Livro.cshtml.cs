ec8e65bc555ca3ce3b7a1469e5c945d3e37d86ed
FeHunter <hunterfe1111@gmail.com>
2024-08-11 09:29:48 -0300

    Criando Componente Livro e adicionando dados a página do livro, adicionado também a rota até a página do Livro.

--- Pages/Livro.cshtml.cs
+++ Pages/Livro.cshtml.cs
@@ -5,8 +5,19 @@ namespace TP1_C_Sharp_ASP.NET.Pages
 {
     public class LivroModel : PageModel
     {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoDePublicacao { get; set; }
+        public double Preco { get; set; }
+
         public void OnGet()
         {
+            Titulo = "O Senhor dos Anéis";
+            Autor = "J.R.R. Tolkien";
+            AnoDePublicacao = 1954;
+            Preco = 79.90;
         }
     }
 }
+
+// "O Senhor dos Anéis", "J.R.R. Tolkien", 1954, 79.90
