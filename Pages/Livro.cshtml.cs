42ba20f594de89696817ba07d88d126ffe591f7b
FeHunter <hunterfe1111@gmail.com>
2024-08-11 09:14:28 -0300

    first commit

---
+++ Pages/Livro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TP1_C_Sharp_ASP.NET.Pages
+{
+    public class LivroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
